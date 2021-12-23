d72abadcb879cb8adb97e516afa587a617adbc09
CollinEdward <manorengaard@icloud.com>
2021-12-23 01:58:39 +0100

    Commit testing.

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ sr_version = sr.__version__
 User_name = platform.node()
 
 def script() -> None:
+
+    # Veriables for voice recignition
+    r = sr.Recognizer()
+    r.recognize_google()
+    mic = sr.Microphone()
+    
+
     print(f"Voice recognition version {sr_version}")
     print(f"Welcome {User_name} to the HelloUser script")
 
