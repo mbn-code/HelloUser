057163b13311e8a7cbb90d90fc5a6674d57a1f55
CollinEdward <manorengaard@icloud.com>
2021-12-23 01:46:14 +0100

    Speech recognition added.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import random
 import platform
+import speech_recognition as sr
+
+
+sr_version = sr.__version__
 
 User_name = platform.node()
 
 def script() -> None:
-    print(f"Welcome {User_name}")
+    print(f"Voice recognition version {sr_version}")
+    print(f"Welcome {User_name} to the HelloUser script")
+
 
 
 if __name__ == "__main__":
